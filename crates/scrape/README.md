6b2e5d54f7a1b3880a75a873e6098d291abf5afc
yggverse <yggverse@project>
2026-06-12 02:54:12 +0000

    return `btracker-scrape` shared dependency, but with `btpeer` async backend, build-in I2P support and future cache features

---
+++ crates/scrape/README.md
@@ -0,0 +1,12 @@
+# btracker-scrape
+
+[![Dependencies](https://deps.rs/repo/github/YGGverse/btracker-scrape/status.svg)](https://deps.rs/repo/github/YGGverse/btracker-scrape)
+[![crates.io](https://img.shields.io/crates/v/btracker-scrape.svg)](https://crates.io/crates/btracker-scrape)
+
+Shared BitTorrent scrape API for the βtracker project components
+
+## Install
+
+``` bash
+cargo add btracker-scrape
+```
---
+++ crates/scrape/src/lib.rs
@@ -0,0 +1,87 @@
+use anyhow::{Result, bail};
+use btpeer::http::response::scrape::Total;
+use librqbit_core::Id20;
+use std::time::Duration;
+use url::Url;
+
+struct Scrape {
+    proxy: Option<String>,
+    timeout: Duration,
+    tracker: Url,
+}
+
+impl Scrape {
+    pub fn new(
+        tracker: Url,
+        timeout: u64,
+        proxy: Option<String>,
+        proxy_i2p: Option<String>,
+    ) -> Result<Self> {
+        if !tracker.scheme().starts_with("http") {
+            bail!("HTTP trackers only!")
+        }
+        Ok(Self {
+            proxy: if tracker
+                .host_str()
+                .expect("Host is required")
+                .ends_with(".i2p")
+            {
+                if proxy_i2p.is_none() {
+                    bail!("I2P proxy is required for tracker `{tracker}`")
+                }
+                proxy_i2p
+            } else {
+                proxy
+            },
+            timeout: Duration::from_secs(timeout),
+            tracker,
+        })
+    }
+
+    pub async fn get(&self, id20: Id20) -> Result<Total> {
+        Ok(btpeer::http::scrape(
+            &btpeer::http::query::Scrape::new(self.tracker.as_str(), Some(&[id20.0]))?,
+            self.timeout,
+            self.proxy.as_deref(),
+        )
+        .await?
+        .total)
+    }
+}
+
+pub struct Buffer(Vec<Scrape>);
+
+impl Buffer {
+    pub fn new(
+        trackers: Vec<Url>,
+        timeout: u64,
+        proxy: Option<&Url>,
+        proxy_i2p: Option<&Url>,
+    ) -> Result<Self> {
+        let mut this = Vec::with_capacity(trackers.len());
+
+        for url in trackers {
+            this.push(Scrape::new(
+                url,
+                timeout,
+                proxy.as_ref().map(|p| p.to_string()),
+                proxy_i2p.as_ref().map(|p| p.to_string()),
+            )?)
+        }
+
+        Ok(Self(this))
+    }
+
+    pub async fn get(&self, id20: Id20) -> Result<Total> {
+        let mut total = Total::default();
+
+        for this in self.0.iter() {
+            let result = this.get(id20).await?;
+            total.complete += result.complete;
+            total.downloaded += result.downloaded;
+            total.incomplete += result.incomplete;
+        }
+
+        Ok(total)
+    }
+}
